347b563e1ced4a81bd5eb2d7269eecbf3cc2de0d
b7c <58299468+b7c@users.noreply.github.com>
2023-10-31 01:31:42 +1300

    improve extract command output

--- cmd/swfx/cmd/extract/cmd.go
+++ cmd/swfx/cmd/extract/cmd.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"image/png"
 	"os"
-	"path"
+	"path/filepath"
 	"strconv"
+	"strings"
 
 	"github.com/gabriel-vasile/mimetype"
 
@@ -13,10 +14,11 @@ import (
 
 	"github.com/b7c/swfx"
 	"github.com/b7c/swfx/cmd/swfx/cmd"
+	"github.com/b7c/swfx/tagcode"
 )
 
 var (
-	outputDir     string
+	baseOutDir    string
 	quiet         bool
 	extractAll    bool
 	extractImages bool
@@ -25,50 +27,31 @@ var (
 	extractCount int
 )
 
+var extractCounts = make(map[tagcode.TagCode]int)
+
 var extractCmd = &cobra.Command{
 	Use:     "extract",
 	Aliases: []string{"x"},
-	Short:   "A brief description of your command",
+	Short:   "Extracts resources from tags.",
 	Run: func(cmd *cobra.Command, args []string) {
-
-		extractAll = !(extractBinary || extractImages)
-
-		err := os.MkdirAll(outputDir, 0755)
-		if err != nil {
-			fmt.Fprintf(cmd.ErrOrStderr(), "failed to create output directory: %s\n", err)
+		if len(args) == 0 {
+			cmd.Help()
 			return
 		}
 
+		extractAll = !(extractBinary || extractImages)
+
 		for _, fileName := range args {
-			f, err := os.Open(fileName)
+			cmd.Println(fileName)
+			err := extractSwf(cmd, fileName)
 			if err != nil {
-				fmt.Fprintf(cmd.ErrOrStderr(), "%s: failed to open file\n", fileName)
-				continue
+				cmd.Println(err)
 			}
-			defer f.Close()
-
-			swf, err := swfx.ReadSwf(f)
-			if err != nil {
-				fmt.Fprintf(cmd.ErrOrStderr(), "%s: failed to read swf - %s\n", fileName, err)
-				continue
-			}
-
-			for _, tag := range swf.Tags {
-				switch tag := tag.(type) {
-				case *swfx.DefineBitsLossless2:
-					if extractAll || extractImages {
-						extractBitsLossless2(swf, tag)
-					}
-				case *swfx.DefineBitsJpeg2:
-					if extractAll || extractImages {
-						extractBitsJpeg2(swf, tag)
-					}
-				case *swfx.DefineBinaryData:
-					if extractAll || extractBinary {
-						extractBinaryData(swf, tag)
-					}
-				}
-			}
+		}
+		
+		cmd.Printf("\nExtracted %d file%s.\n", extractCount, pluralize(extractCount))
+		for code, count := range extractCounts {
+			cmd.Printf("* %s: %d\n", code, count)
 		}
 	},
 }
@@ -76,14 +59,85 @@ var extractCmd = &cobra.Command{
 func init() {
 	cmd.RootCmd.AddCommand(extractCmd)
 
-	extractCmd.Flags().StringVarP(&outputDir, "output", "o", ".", "The output directory.")
+	extractCmd.Flags().StringVarP(&baseOutDir, "output", "o", "", "The output directory. Creates a directory with the same name as the SWF file without the extension by default.")
 	extractCmd.Flags().BoolVarP(&extractAll, "all", "a", false, "Extract all resources.")
 	extractCmd.Flags().BoolVarP(&extractImages, "images", "i", false, "Extract images.")
 	extractCmd.Flags().BoolVarP(&extractBinary, "binary", "b", false, "Extract binary data.")
 	extractCmd.Flags().BoolVarP(&quiet, "quiet", "q", false, "Don't output filenames.")
 }
 
-func extractBitsLossless2(swf *swfx.Swf, tag *swfx.DefineBitsLossless2) error {
+func pluralize(n int) string {
+	if n == 1 {
+		return ""
+	}
+	return "s"
+}
+
+func extractSwf(cmd *cobra.Command, fileName string) error {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	
+	stat, err := f.Stat()
+	if err != nil {
+		return err
+	}
+	
+	if stat.IsDir() {
+		return fmt.Errorf("input must be a file")
+	}
+
+	outDir := baseOutDir
+	if outDir == "" {
+		index := strings.LastIndex(fileName, ".")
+		if index != -1 {
+			outDir = fileName[:index]
+		} else {
+			outDir = fileName + "_extracted"
+		}
+	}
+
+	err = os.MkdirAll(outDir, 0755)
+	if err != nil {
+		return err
+	}
+
+	swf, err := swfx.ReadSwf(f)
+	if err != nil {
+		return err
+	}
+
+	for _, tag := range swf.Tags {
+		switch tag := tag.(type) {
+		case *swfx.DefineBitsLossless2:
+			if extractAll || extractImages {
+				err := extractBitsLossless2(outDir, swf, tag)
+				if err != nil {
+					cmd.PrintErrf("failed to extract image from character #%d: %s", tag.CharacterId(), err)
+				}
+			}
+		case *swfx.DefineBitsJpeg2:
+			if extractAll || extractImages {
+				err := extractBitsJpeg2(outDir, swf, tag)
+				if err != nil {
+					cmd.PrintErrf("failed to extract image from character #%d: %s", tag.CharacterId(), err)
+				}
+			}
+		case *swfx.DefineBinaryData:
+			if extractAll || extractBinary {
+				err := extractBinaryData(outDir, swf, tag)
+				if err != nil {
+					cmd.PrintErrf("failed to extract image from character #%d: %s", tag.CharacterId(), err)
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func extractBitsLossless2(outDir string, swf *swfx.Swf, tag *swfx.DefineBitsLossless2) error {
 	var names []string
 	var ok bool
 	if names, ok = swf.ReverseSymbols[tag.CharacterId()]; !ok {
@@ -91,22 +145,22 @@ func extractBitsLossless2(swf *swfx.Swf, tag *swfx.DefineBitsLossless2) error {
 	}
 
 	for _, name := range names {
-		outputFile := path.Join(outputDir, name+".png")
+		outputFile := filepath.Join(outDir, name+".png")
 		img, err := tag.Decode()
 		if err != nil {
-			return fmt.Errorf("failed to decode tag (%d): %s",
-				tag.CharacterId(), err)
+			return err
 		}
 		f, err := os.OpenFile(outputFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0755)
 		if err != nil {
-			return fmt.Errorf("failed to open file: %q", outputFile)
+			return err
 		}
 		defer f.Close()
 		err = png.Encode(f, img)
 		if err != nil {
-			return fmt.Errorf("failed to encode image: %s", err)
+			return err
 		}
 		extractCount++
+		extractCounts[tag.Code()]++
 		if !quiet {
 			fmt.Println(outputFile)
 		}
@@ -114,12 +168,13 @@ func extractBitsLossless2(swf *swfx.Swf, tag *swfx.DefineBitsLossless2) error {
 	return nil
 }
 
-func extractBitsJpeg2(swf *swfx.Swf, tag *swfx.DefineBitsJpeg2) error {
+func extractBitsJpeg2(outDir string, swf *swfx.Swf, tag *swfx.DefineBitsJpeg2) error {
 	var names []string
 	var ok bool
 	if names, ok = swf.ReverseSymbols[tag.CharacterId()]; !ok {
 		names = []string{fmt.Sprintf("%d", tag.CharacterId())}
 	}
+
 	var ext string
 	switch tag.ImageType() {
 	case swfx.Jpeg:
@@ -131,18 +186,20 @@ func extractBitsJpeg2(swf *swfx.Swf, tag *swfx.DefineBitsJpeg2) error {
 	default:
 		return fmt.Errorf("unknown image type")
 	}
+
 	for _, name := range names {
-		outputFile := path.Join(outputDir, name+ext)
+		outputFile := filepath.Join(outDir, name+ext)
 		f, err := os.OpenFile(outputFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0755)
 		if err != nil {
-			return fmt.Errorf("failed to open file: %q", outputFile)
+			return err
 		}
 		defer f.Close()
-		n, err := f.Write(tag.ImageData)
-		if n < len(tag.ImageData) {
-			return fmt.Errorf("failed to write all data")
+		_, err = f.Write(tag.ImageData)
+		if err != nil {
+			return err
 		}
 		extractCount++
+		extractCounts[tag.Code()]++
 		if !quiet {
 			fmt.Println(outputFile)
 		}
@@ -150,7 +207,7 @@ func extractBitsJpeg2(swf *swfx.Swf, tag *swfx.DefineBitsJpeg2) error {
 	return nil
 }
 
-func extractBinaryData(swf *swfx.Swf, tag *swfx.DefineBinaryData) error {
+func extractBinaryData(outDir string, swf *swfx.Swf, tag *swfx.DefineBinaryData) error {
 	var names []string
 	var ok bool
 	if names, ok = swf.ReverseSymbols[tag.CharacterId()]; !ok {
@@ -161,7 +218,7 @@ func extractBinaryData(swf *swfx.Swf, tag *swfx.DefineBinaryData) error {
 	ext := mtype.Extension()
 
 	for _, name := range names {
-		outputFile := path.Join(outputDir, name+ext)
+		outputFile := filepath.Join(outDir, name+ext)
 
 		f, err := os.OpenFile(outputFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0755)
 		if err != nil {
@@ -169,11 +226,12 @@ func extractBinaryData(swf *swfx.Swf, tag *swfx.DefineBinaryData) error {
 		}
 		defer f.Close()
 
-		n, err := f.Write(tag.Data)
-		if n < len(tag.Data) {
-			return fmt.Errorf("failed to write all data")
+		_, err = f.Write(tag.Data)
+		if err != nil {
+			return err
 		}
 		extractCount++
+		extractCounts[tag.Code()]++
 		if !quiet {
 			fmt.Println(outputFile)
 		}
